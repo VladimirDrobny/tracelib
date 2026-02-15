167ac1d33f78c57be2175f8b15c9a3acdb888e96
Vladimir Drobny <xdrobny2@fi.muni.cz>
2026-02-15 22:49:32 +0100

    Change CCTNode::children to abseil::btree_map

--- include/tracelib/callingContextTree.hpp
+++ include/tracelib/callingContextTree.hpp
@@ -16,6 +16,7 @@
 #include <boost/serialization/split_member.hpp>
 #include <boost/serialization/export.hpp>
 #include <boost/serialization/unique_ptr.hpp>
+#include <absl/container/btree_map.h>
 
 #include "utils.hpp"
 
@@ -59,7 +60,7 @@ public:
     /**
      * @brief The children of this node in a map where the key is nodes function name.
      */
-    std::unordered_map<functionIdType, nodeIdType> children = {};
+    absl::btree_map<functionIdType, nodeIdType> children = {};
 
     /**
      * @brief Creates an empty node.
